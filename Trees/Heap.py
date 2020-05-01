4893f514ab7645c179de7ae8f6e42f4d24755119
Jennifer Bernardez <jbernardez22@cmc.edu>
2020-04-30 23:58:40 -0700

    Heaps finished

--- Trees/Heap.py
+++ Trees/Heap.py
@@ -83,7 +83,7 @@ class Heap(BinaryTree):
         '''
         if self.root is None:
             self.root = Node(value)
-            self.root.descendents = 1  #tracks the size of the ttree that is rooted at that node. since this is rooted from binary tree, you can use the size function from binarytree and you'll get the right results but wrong runtime. so instead of using size willl need to do this in the insert function. on each of your nodes, create another attribute for that node that has the size for taht node.  in your _insert everytime you create a new node value, you increment its values down. want to calculate how many children it has total . decendents is one for the node itself and one for every child on the left and the right 
+            self.root.descendents = 1
         else:
             Heap._insert(value, self.root)
 
@@ -98,8 +98,6 @@ class Heap(BinaryTree):
         node.descendents +=1
 
         binary = "{0:b}".format(node.descendents) #takes descendents and prints it in a string of binary. the first digit is gauranteed to be a 1. converts it into a single string
-#for debuggigng print this
-       # print('binary = ', binary)
         if binary[1] == '0': #need to take this code to adapt it to go to the right direction. right now goes to left
             if node.left is None: #means we have gotten to the bottom of the tree, there is no child we are at a leaf node so at this point we have to add our node
                 node.left = Node(value)
@@ -120,11 +118,6 @@ class Heap(BinaryTree):
                 node.value, node.right.value = node.right.value, node.value
 
     
-    def size (node):  #what is size trying to find the size of
-        if node is None:
-            return 0
-
-
 
 
     def insert_list(self, xs):
@@ -168,13 +161,9 @@ class Heap(BinaryTree):
             last_element = Heap._remove_last_element(self.root)
             self.root.value = last_element
             Heap._swap(self.root)
-            #remove last element
-            #set root.value to the return value of remove lst element
-            #run swap function
-            print("done with remove min", self)
 
     @staticmethod 
-    def _remove_last_element(node): #where should i subtract from descendants? 
+    def _remove_last_element(node):  
         binary = "{0:b}".format(node.descendents)
         node.descendents = node.descendents - 1
         if binary[1] == '0':
@@ -194,11 +183,9 @@ class Heap(BinaryTree):
 
     @staticmethod
     def _swap(node): 
-        print("before if", node) #then print node within every if statement
         if node.left and node.right:
             if node.left.value <= node.right.value and node.left.value < node.value:
                 node.value, node.left.value = node.left.value, node.value
-                print("should be left side", node)
                 Heap._swap(node.left)
             elif node.right.value <=  node.left.value and node.right.value < node.value:
                 node.value, node.right.value = node.right.value, node.value
@@ -211,12 +198,6 @@ class Heap(BinaryTree):
             if node.value > node.right.value:
                 node.value, node.right.value = node.right.value, node.value
                 Heap._swap(node.right)
-        print("made it through swap", node) 
-    #this function finds the value and returns it
-    #remove min does the actual swapping
 
-#second function takes node in and does not return anything, it will  be editing the tree
 
-            #need additionaly helper funcitons beyong those two, just have root value 
-            #remove min has all the complications 
 
